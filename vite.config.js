2621b3f43ee69178e3b6e23a9895a614bbb184fb
tamer tamer <amr.dawoodi@hotmail.com>
2024-10-14 02:27:34 +0300

    fiximagefour

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Adjust this if you are deploying to a subfolder or custom domain
   build: {
-    outDir: 'dist', // Default output directory
-  },
+    assetsInlineLimit: 0, // Disable inlining of images into the JavaScript bundle
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]' // Place images in assets folder
+      }
+    }
+  }
 })
  
