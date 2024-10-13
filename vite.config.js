fe0c091bc6f5103e08d649dffd542ff94a17d566
tamer tamer <amr.dawoodi@hotmail.com>
2024-10-14 02:24:31 +0300

    fiximagethree

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Adjust this if you are deploying to a subfolder or custom domain
+  build: {
+    outDir: 'dist', // Default output directory
+  },
 })
  
