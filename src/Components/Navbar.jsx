cbbcdb4be8132837f574a1b2915b7b913624e9fe
tamer tamer <amr.dawoodi@hotmail.com>
2024-10-16 01:52:43 +0300

    add loader

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,6 +23,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 function ThemeSwitcher({ SetDarkModeValue   }) {
 
   const [darkMode, setDarkMode] = useState(false);
+  
 
 
   const [navigation, setNavigation] = useState([
@@ -33,6 +34,10 @@ function ThemeSwitcher({ SetDarkModeValue   }) {
     { name: 'Contact', href: '#Footer', current: false },
   ]);
 
+  const [loading, setloading] = useState([
+    { loadingValue: false },
+
+  ]);
   const handleClick = (clickedItem) => {
     setNavigation((prevNavigation) =>
       prevNavigation.map((item) =>
@@ -41,7 +46,29 @@ function ThemeSwitcher({ SetDarkModeValue   }) {
           : { ...item, current: false } // Set current to false for other items
       )
     );
+    
+    
+    
   };
+
+
+  const HandeLoading = (clickedItem) => {
+    setloading((prevNavigation) =>
+      prevNavigation.map((item) =>
+        item.name === clickedItem.name
+          ? { ...item, loadingValue: true } // Set current to true for the clicked item
+          : { ...item, loadingValue: false } // Set current to false for other items
+      )
+    )
+    
+    setTimeout(() => {
+      setloading((prevNavigation)=> prevNavigation.map((item)=>
+      item.loadingValue === clickedItem.name ?  { ...item, loadingValue: false }  : { ...item, loadingValue: false} ))
+    }, 4000);
+
+    
+  };
+
   
 
   useEffect(() => {
@@ -324,20 +351,28 @@ function ThemeSwitcher({ SetDarkModeValue   }) {
              //end  svg
              }
         </button>    
+            <div className="relative ml-3">         
+              <div className=' '>
 
-
-
-        {/* <a href="https://online.flippingbook.com/view/747196872/" class="fbo-embed" data-fbo-id="81805f0d4f" data-fbo-ratio="3:2" data-fbo-lightbox="yes" data-fbo-width="100%" data-fbo-height="auto" data-fbo-version="1" style="max-width: 100%">omar dwaoodi resume</a><script async defer src="https://online.flippingbook.com/EmbedScriptUrl.aspx?m=redir&hid=747196872"></script> */}
-            
-        
-            <div className="relative ml-3">   
-                           
-              <div>
-                {/* <a href="/src/CvFile/a'mr dawoodi resume.pdf" download={"a'mr dawoodi resume.pdf"}> */}
                 <a href="https://drive.usercontent.google.com/u/0/uc?id=1F3iQxghoAlSynj5zDF1VDbk1WKRhxJoi&export=download" download={"a'mr dawoodi resume.pdf"}>
-                  <button className={`relative flex rounded-full ${darkMode?'bg-white text-black font-bold':'bg-darker text-white'}  text-sm rounded-lg p-2  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 `}>
-                      Download CV     
-                  </button>
+                {loading.map((item) => (        
+
+                    <button 
+                    onClick={() => HandeLoading(item)}
+                    className={`relative flex rounded-full ${darkMode?'bg-white text-black font-bold':'bg-darker text-white'}  text-sm rounded-lg p-2  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 `}>
+                    {item.loadingValue ? 
+                    
+                      <div role="status" className='px-2'>
+                      <svg aria-hidden="true" class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                      </svg>
+                      <span class="sr-only">Loading...</span>
+                    </div>
+                        : ''}
+                        {item.loadingValue? 'Please Wait':'Download CV'}     
+                    </button>
+                  ))}
                 </a>
               </div>
             </div>
